e97af958735adf2238a49def03ca58b4aee358fc
ykw <1794093413@qq.com>
2021-03-07 22:06:53 +0800

    修改api

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/': {
-        target: 'http://39.96.92.113:3000/',
+        target: 'http://127.0.0.1:3000/',
         changeOrigin: true,
         secure: true
       }
