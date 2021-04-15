6dec24dc59b7bccff5ef825735814162920bb110
ykw <1794093413@qq.com>
2021-04-16 01:12:02 +0800

    修改proxy

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   publicPath: './', // 基本路径
   outputDir: 'music', // 输出文件目录
-  devServer: {
-    proxy: {
-      '/': {
-        target: 'http://39.96.92.113:3000/',
-        changeOrigin: true,
-        secure: true
-      }
-    }
-  },
   configureWebpack: config => {
     let settings = {}
     if (process.env.NODE_ENV === 'production') {
